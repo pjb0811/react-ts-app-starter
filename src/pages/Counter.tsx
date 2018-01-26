3b47a8ce109266c193c32f87f770a5c577ce238d
jbpark <jbpark@wemakeprice.com>
2018-01-26 10:48:39 +0900

    setting redux ducks structure

--- src/pages/Counter.tsx
+++ src/pages/Counter.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
-// import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-// import * as counterActions from '../redux/modules/counter';
+import { bindActionCreators } from 'redux';
+import * as counterActions from '../redux/modules/counter';
 
 interface Props {
-  // CounterActions: {};
+  CounterActions: typeof counterActions;
   num: number;
 }
 
@@ -14,13 +14,12 @@ interface State {
 
 class Counter extends React.Component<Props> {
   render() {
-    const { num } = this.props;
-    console.log(this.props);
+    const { num, CounterActions } = this.props;
     return (
       <div>
       <h1>{num}</h1>
-      {/* <button onClick={CounterActions.increment}>+</button>
-      <button onClick={CounterActions.decrement}>-</button> */}
+      <button onClick={CounterActions.increment}>+</button>
+      <button onClick={CounterActions.decrement}>-</button>
       </div>
     );
   }
@@ -30,35 +29,7 @@ export default connect(
   (state: State) => ({
     num: state.counter
   }),
+  (dispatch) => ({
+    CounterActions: bindActionCreators(counterActions, dispatch)
+  })
 )(Counter);
-
-/*
-import * as React from 'react';
-import { connect } from 'react-redux';
-
-interface CounterProps {
-  value: number;
-}
-
-interface CounterState {
-  counter: {
-    value: number;
-  };
-}
-
-class Counter extends React.Component<CounterProps, CounterState> {
-  render() {
-    return (
-      <h1>VALUE: {this.props.value}</h1>
-    );
-  }
-}
-
-let mapStateToProps = (state: CounterState) => {
-  return {
-    value: state.counter.value
-  };
-};
-
-export default connect(mapStateToProps)(Counter);
-*/
